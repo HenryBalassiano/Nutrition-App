c3a1aa26f496fe4a59b262d131640f3f63d586ac
Henry Balassiano <henrybee321@gmail.com>
2020-11-11 03:09:11 -0500

    Updates

---
+++ precache-manifest.02090d60eec2520876a338b512c27d8f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc55a821591fcb6035fd155204b456eb",
+    "url": "/Nutrition-App/index.html"
+  },
+  {
+    "revision": "fddaaa5ceceae1ee3c98",
+    "url": "/Nutrition-App/static/css/main.e73fb306.chunk.css"
+  },
+  {
+    "revision": "7ba470021688b10cd40e",
+    "url": "/Nutrition-App/static/js/2.ca036947.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Nutrition-App/static/js/2.ca036947.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fddaaa5ceceae1ee3c98",
+    "url": "/Nutrition-App/static/js/main.8c9c4c41.chunk.js"
+  },
+  {
+    "revision": "97812c7ec889a2497e36",
+    "url": "/Nutrition-App/static/js/runtime-main.2a9520c2.js"
+  }
+]);
